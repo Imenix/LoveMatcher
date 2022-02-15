527c840bac28d1e24247f37543d71eb5b1115583
MahYus97 <77026805+MahYus97@users.noreply.github.com>
2022-02-15 14:18:45 +0100

    Rättningar

---
+++ LoveMatcher/Menu/Back.cs
@@ -0,0 +1,15 @@
+public class Back
+{
+    public void MainMenu()
+    {
+        var choice = Console.ReadKey();
+        switch(choice.Key)
+        {
+        case ConsoleKey.D1:
+        break;
+
+        case ConsoleKey.D2:
+        break;
+        }
+    }
+}
---
+++ LoveMatcher/Menu/Front.cs
@@ -0,0 +1,21 @@
+public class Front
+{
+	public void MainMenuText()
+    {
+        Box.Simple(new string[]
+           {"Choose an option",
+            "[1]  Start comparing",
+            "[2]  Exit",
+           });     
+    }
+
+    public void CompareMenu()
+    {
+
+    }
+
+    public void ResultMenu()
+    {
+
+    }
+}
