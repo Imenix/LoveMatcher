a76fae783347b6d60b60f489afc3f417ef5f81bd
Tiia Tupamäki <tiia.tupamaki@gmail.com>
2022-02-15 13:39:51 +0100

    CompareName: la till kommentarer i metoder

--- LoveMatcher/Helper/Comparing/CompareName.cs
+++ LoveMatcher/Helper/Comparing/CompareName.cs
@@ -1,30 +1,53 @@
-//skillnad på längd
-/// <summary>
-/// Calculates the difference 
-/// </summary>
-public int Length()
+namespace LoveMatcher.Helper.Comparing
 {
+    public class CompareName
+    {
+        //skillnad på längd
+        /// <summary>
+        /// Calculates the difference between lenght of the names
+        /// </summary>
+        public int Length()
+        {
+            return 0;
+        }
 
-}
+        //är längderna udda eller jämna        
+        /// <summary>
+        /// Checks if lenght of the name is even or not
+        /// </summary>
+        /// <returns></returns>
+        public bool LenghtIsEven()
+        {
+            return 0;
+        }
 
-//är längderna udda eller jämna
-public int OddOrEvenLenght()
-{
-    
-}
+        //skillnad i mängd vokaler        
+        /// <summary>
+        /// Checks the number of vocals in the name.
+        /// </summary>
+        /// <returns></returns>
+        public int NumberOfVocals()
+        {
+            return 0;
+        }
 
-//skillnad i mängd vokaler
-public int NumberOfVocals()
-{
-    
-}
-//skillnad i mängd konsonanter
-public int NumberOfConsonants()
-{
-    
-}
-//Skillnad i mängd dubbla bokstäver
-public int NumberOfSameLetters()
-{
-    
+        //skillnad i mängd konsonanter        
+        /// <summary>
+        /// Checks the number of consonants in the name.
+        /// </summary>
+        public int NumberOfConsonants()
+        {
+            return 0;
+        }
+
+        //Skillnad i mängd dubbla bokstäver        
+        /// <summary>
+        /// Checks how many same letters the names have. 
+        /// </summary>
+        /// <returns></returns>
+        public int NumberOfSameLetters()
+        {
+            return 0;
+        }
+    }
 }
