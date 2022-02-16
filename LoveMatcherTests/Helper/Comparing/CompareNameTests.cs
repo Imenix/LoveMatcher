dc3dc357e6a0c1cb8f74c452f23fea99150611e5
Mikael Hargeus <Mikael.Hargeus@live.se>
2022-02-16 11:22:13 +0100

    CreatedTestClass

---
+++ LoveMatcherTests/Helper/Comparing/CompareNameTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LoveMatcher.Helper.Comparing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoveMatcher.Helper.Comparing.Tests
+{
+    [TestClass()]
+    public class CompareNameTests
+    {
+        [TestMethod()]
+        public void LengthTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void LengthIsEvenTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void NumberOfVocalsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void NumberOfConsonantsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void NumberOfSameLettersTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
