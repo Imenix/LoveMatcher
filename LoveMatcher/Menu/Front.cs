64a822dae170df9d05af0fa09fa006d44205cfe4
Max Ekholm <90773776+YellowHyena@users.noreply.github.com>
2022-02-16 14:03:50 +0100

    vetefan

--- LoveMatcher/Menu/Front.cs
+++ LoveMatcher/Menu/Front.cs
@@ -1,20 +1,24 @@
 public class Front
 {
-	public void MainMenuText()
+	public static void MainMenuText()
     {
         Box.Simple(new string[]
-           {"Choose an option",
-            "[1]  Start comparing",
-            "[2]  Exit",
-           });
+        {"Choose an option",
+         "[1]  Start comparing",
+         "[2]  Exit",
+        });
     }
 
-    public void CompareMenu()
+    public static void CompareMenuText_StageOne()
     {
-
+        Box.Simple(new string[]
+        {"Name:",
+         "Age(yyyy/mm/dd):",
+         "[btn] Return"
+        });
     }
 
-    public void ResultMenu()
+    public void ResultMenuText()
     {
 
     }
