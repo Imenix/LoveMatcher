0f871b9253c2a4c5b985d7b55bf1395ef566d8c7
Tiia Tupamäki <tiia.tupamaki@gmail.com>
2022-02-17 12:43:55 +0100

    Ändrade klassnamnet till TotalScore

--- LoveMatcher/Helper/ScoreCounter/TotalScore.cs
+++ LoveMatcher/Helper/ScoreCounter/TotalScore.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LoveMatcher.Helper.PointCounter
+namespace LoveMatcher.Helper.ScoreCounter
 {
-    internal class Points
+    internal class TotalScore
     {
         //lagrar totala poäng
     }
