e88a47270fa9e5879b230e61d55565208ba4ae99
Tiia Tupamäki <tiia.tupamaki@gmail.com>
2022-02-18 11:26:30 +0100

    Metod för ScoreGiver

--- LoveMatcher/Helper/ScoreCounter/ScoreGiver.cs
+++ LoveMatcher/Helper/ScoreCounter/ScoreGiver.cs
@@ -8,6 +8,31 @@ namespace LoveMatcher.Helper.ScoreCounter
 {
     internal class ScoreGiver
     {
+        CompareAge compare = new();
+        TotalScore totalScore = new();
+        public int AgeDifference_Score()
+        {
+            return 0;
+        }
 
+        public void IsOver17_Score(Person person1, Person person2)
+        {
+            var Age1 = compare.IsOver17(person1.Age);
+            var Age2 = compare.IsOver17(person2.Age);
+            
+            if ((Age1 && Age2) || (!Age1 && !Age2))
+            {
+                totalScore.AddToTotal(10);
+            }
+            else
+            {
+                totalScore.SubstractFromTotal(5);
+            }
+        }
+
+        public int AgeIsEvenNumber_Score()
+        {
+            return 0;
+        }
     }
 }
