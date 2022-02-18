63804a4aef86b64c7d5742a7f112bd842d0e7205
Mikael Hargeus <Mikael.Hargeus@live.se>
2022-02-18 11:38:35 +0100

    AddedMethodsScoreGiver

--- LoveMatcher/Helper/ScoreCounter/ScoreGiver.cs
+++ LoveMatcher/Helper/ScoreCounter/ScoreGiver.cs
@@ -9,5 +9,26 @@ namespace LoveMatcher.Helper.ScoreCounter
     internal class ScoreGiver
     {
 
+        public int LengthNameScore(Person person1, Person person2)
+        {
+            return 0;
+        }
+        public int LengthIsEvenScore()
+        {
+            return 0;
+        }
+        public int NumberOfVowelsScore()
+        {
+            return 0;
+        }
+        public int NumberOfConsonantsScore()
+        {
+            return 0; 
+        }
+        public int NumberOfSameLettersScore()
+        {
+            return 0;
+        }
+
     }
 }
