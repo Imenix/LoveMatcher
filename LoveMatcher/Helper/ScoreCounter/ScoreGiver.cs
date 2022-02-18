779d67e04a69b85dc322713a988025caf95f19a6
Tiia Tupamäki <tiia.tupamaki@gmail.com>
2022-02-18 15:40:04 +0100

    Testklassen för ScoreGiver

--- LoveMatcher/Helper/ScoreCounter/ScoreGiver.cs
+++ LoveMatcher/Helper/ScoreCounter/ScoreGiver.cs
@@ -6,46 +6,45 @@ using System.Threading.Tasks;
 
 namespace LoveMatcher.Helper.ScoreCounter
 {
-    internal class ScoreGiver
+    public class ScoreGiver
     {
         readonly CompareAge compare = new();
-        TotalScore totalScore = new();
-        public void AgeDifference_Score(Person person1, Person person2)
+        readonly TotalScore totalScore = new();
+        public int AgeDifference_Score(Person person1, Person person2)
         {
             var diff = compare.AgeDifference(person1.Age, person2.Age);
 
-            _ = diff switch
+            return diff switch
             {
                 0 => totalScore.AddToTotal(15),
                 > 0 and < 10 => totalScore.AddToTotal(10),
                 >= 10 and < 20 => totalScore.AddToTotal(10),
                 _ => totalScore.SubstractFromTotal(5),
             };
-
         }
-        public void IsOver17_Score(Person person1, Person person2)
+        public int IsOver17_Score(Person person1, Person person2)
         {
             var age1 = compare.IsOver17(person1.Age);
             var age2 = compare.IsOver17(person2.Age);
-            GiveScore(age1, age2);
+            return GiveScore(age1, age2);
         }
 
-        public void AgeIsEvenNumber_Score(Person person1, Person person2)
+        public int AgeIsEvenNumber_Score(Person person1, Person person2)
         {
             var age1 = compare.AgeIsEvenNumber(person1.Age);
             var age2 = compare.AgeIsEvenNumber(person2.Age);
-            GiveScore(age1, age2);
+            return GiveScore(age1, age2);
         }
 
-        private void GiveScore(bool age1, bool age2)
+        private int GiveScore(bool age1, bool age2)
         {
             if ((age1 && age2) || (!age1 && !age2))
             {
-                totalScore.AddToTotal(10);
+                return totalScore.AddToTotal(10);
             }
             else
             {
-                totalScore.SubstractFromTotal(5);
+                return totalScore.SubstractFromTotal(5);
             }
         }
     }
---
+++ LoveMatcherTests/Helper/ScoreCounter/ScoreGiverTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LoveMatcher.Helper.ScoreCounter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoveMatcher.Helper.ScoreCounter.Tests
+{
+    [TestClass()]
+    public class ScoreGiverTests
+    {
+        [TestMethod()]
+        public void AgeDifference_ScoreTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IsOver17_ScoreTest(Person person1, Person person2)
+        {
+            person1.Age = 18;
+            person2.Age = 22;
+
+            ScoreGiver test = new();
+            var actual = test.IsOver17_Score(person1, person2);
+
+            var expected = 0;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void AgeIsEvenNumber_ScoreTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
