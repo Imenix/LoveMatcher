180b891e27c833d94b95ff62c171bd117310967d
Tiia Tupamäki <tiia.tupamaki@gmail.com>
2022-02-18 14:49:04 +0100

    ScoreGiver CompareAge metoder färdiga, men inte testade

--- LoveMatcher/Helper/ScoreCounter/ScoreGiver.cs
+++ LoveMatcher/Helper/ScoreCounter/ScoreGiver.cs
@@ -8,18 +8,37 @@ namespace LoveMatcher.Helper.ScoreCounter
 {
     internal class ScoreGiver
     {
-        CompareAge compare = new();
+        readonly CompareAge compare = new();
         TotalScore totalScore = new();
-        public int AgeDifference_Score()
+        public void AgeDifference_Score(Person person1, Person person2)
         {
-            return 0;
-        }
+            var diff = compare.AgeDifference(person1.Age, person2.Age);
 
+            _ = diff switch
+            {
+                0 => totalScore.AddToTotal(15),
+                > 0 and < 10 => totalScore.AddToTotal(10),
+                >= 10 and < 20 => totalScore.AddToTotal(10),
+                _ => totalScore.SubstractFromTotal(5),
+            };
+
+        }
         public void IsOver17_Score(Person person1, Person person2)
         {
             var age1 = compare.IsOver17(person1.Age);
             var age2 = compare.IsOver17(person2.Age);
-            
+            GiveScore(age1, age2);
+        }
+
+        public void AgeIsEvenNumber_Score(Person person1, Person person2)
+        {
+            var age1 = compare.AgeIsEvenNumber(person1.Age);
+            var age2 = compare.AgeIsEvenNumber(person2.Age);
+            GiveScore(age1, age2);
+        }
+
+        private void GiveScore(bool age1, bool age2)
+        {
             if ((age1 && age2) || (!age1 && !age2))
             {
                 totalScore.AddToTotal(10);
@@ -28,11 +47,6 @@ namespace LoveMatcher.Helper.ScoreCounter
             {
                 totalScore.SubstractFromTotal(5);
             }
-        }
-
-        public int AgeIsEvenNumber_Score()
-        {
-            return 0;
         }
     }
 }
