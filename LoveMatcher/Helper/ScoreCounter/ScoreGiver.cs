958bbd11b1643d9e0d2ebbe0f660a876525ef735
Mikael Hargeus <Mikael.Hargeus@live.se>
2022-02-22 23:26:53 +0100

    Added Methods in ScoreGiver

--- LoveMatcher/Helper/ScoreCounter/ScoreGiver.cs
+++ LoveMatcher/Helper/ScoreCounter/ScoreGiver.cs
@@ -9,6 +9,7 @@ namespace LoveMatcher.Helper.ScoreCounter
     internal class ScoreGiver
     {
         CompareAge compare = new();
+        Comparing.CompareName cn = new();
         TotalScore totalScore = new();
         public int AgeDifference_Score()
         {
@@ -19,7 +20,7 @@ namespace LoveMatcher.Helper.ScoreCounter
         {
             var age1 = compare.IsOver17(person1.Age);
             var age2 = compare.IsOver17(person2.Age);
-            
+           
             if ((age1 && age2) || (!age1 && !age2))
             {
                 totalScore.AddToTotal(10);
@@ -34,25 +35,88 @@ namespace LoveMatcher.Helper.ScoreCounter
         {
             return 0;
         }
-        public int LengthNameScore(Person person1, Person person2)
+        public int LengthName_Score(Person person1, Person person2)
         {
+            var totalLength = cn.LengthName(person1.Name)+ cn.LengthName(person2.Name);
+            if (totalLength < 11)
+            {
+                return totalScore.AddToTotal(5);
+            }
+            else if (totalLength < 15)
+            {
+               return totalScore.AddToTotal(10);
+            }
+            else if (totalLength < 20)
+            {
+                return totalScore.AddToTotal(15);
+            }
+            else
+            {
+                return totalScore.AddToTotal(0);
+            }
+        }
+        public int LengthIsEven_Score(Person person1, Person person2)
+        {
+            var isEven1 = (cn.LengthIsEven(person1.Name));
+            var isEven2 = (cn.LengthIsEven(person2.Name));
+
+            if (isEven1 && isEven2)
+            {
+                return totalScore.AddToTotal(10);
+            }
+            else if ((!isEven1 && isEven2) || (!isEven2 || isEven1))
+            {
+                return totalScore.AddToTotal(5);
+            }
+            else { return 0; };
+        }
+        public int NumberOfVowels_Score(Person person1, Person person2)
+        {
+            var count = cn.NumberOfVowels(person1.Name);
+            count += cn.NumberOfVowels(person2.Name);
+
+            if(count >= 10)
+            {
+                return totalScore.AddToTotal(10);
+            }else if (count >= 5)
+            {
+                return totalScore.AddToTotal(5);
+            }else { return 0; };
+
             return 0;
         }
-        public int LengthIsEvenScore()
+        public int NumberOfConsonants_Score(Person person1, Person person2)
         {
-            return 0;
+            var count = cn.NumberOfConsonants(person1.Name);
+            count += cn.NumberOfConsonants(person2.Name);
+
+            if (count >= 10)
+            {
+                return totalScore.AddToTotal(10);
+            }
+            else if (count >= 5)
+            {
+                return totalScore.AddToTotal(5);
+            }
+            else
+            {
+               return totalScore.SubstractFromTotal(5);
+            };
+
         }
-        public int NumberOfVowelsScore()
+        public int NumberOfSameLetters_Score(Person person1, Person person2)
         {
-            return 0;
-        }
-        public int NumberOfConsonantsScore()
-        {
-            return 0; 
-        }
-        public int NumberOfSameLettersScore()
-        {
-            return 0;
+            var counter = cn.NumberOfSameLetters(person1.Name, person2.Name);
+
+            if(counter >= 5)
+            {
+                return totalScore.AddToTotal(10);
+            }else if (counter >= 3)
+            {
+                return totalScore.AddToTotal(5);
+            }
+            else { return 0; };
+            
         }
 
     }
